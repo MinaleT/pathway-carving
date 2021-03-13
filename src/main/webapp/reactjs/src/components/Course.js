f976fa117f04a7641bd045b5901b244182391da0
MinaleT <firewapril25@gmail.com>
2021-03-13 10:08:31 -0800

    "reactjs fontawesome is utilized with jquery.2, popperjs, typescript"

--- src/main/webapp/reactjs/src/components/Course.js
+++ src/main/webapp/reactjs/src/components/Course.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Card, Form, Button, Col} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSave, faPlusSquare} from '@fortawesome/free-solid-svg-icons';
 
-class Course extends React.Component{
+export default class Course extends Component{
+       constructor(props){
+        super(props);
+        this.state = {courseTitle:'', courseCode:'', price:'', languageUsed:''}
+        this.courseChange = this.courseChange.bind(this);
+        this.submitCourse = this.submitCourse.bind(this);
+       }
+
+       submitCourse(event){
+        alert('Course Title: '+this.state.courseTitle+ ', Course Code: '+this.state.courseCode+ ', Language: '+this.state.languageUsed+ ', Price: '+this.state.price);
+        event.preventDefault();
+       }
+
+        courseChange(event){
+            this.setState({
+                [event.target.name]:event.target.value
+            });
+        }
        render(){
-            return(<div>Course </div>);
+            return(
+                <Card className={"border border-dark bg-dark text-white"}>
+                    <Card.Header><FontAwesomeIcon icon={faPlusSquare} /> Add Course</Card.Header>
+                    <Form onSubmit={this.submitCourse} id="courseFormId">
+                        <Card.Body>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridTitle">
+                                    <Form.Label>Course Title</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="courseTitle"
+                                        value={this.state.courseTitle}
+                                        onChange={this.courseChange}
+                                        placeholder="Course Title"
+                                        className={"bg-dark text-white"}/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridCode">
+                                    <Form.Label>Course Code</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="courseCode"
+                                        value={this.state.courseCode}
+                                        onChange={this.courseChange}
+                                        placeholder="Course Code"
+                                        className={"bg-dark text-white"}/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridLanguage">
+                                    <Form.Label>Language</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="languageUsed"
+                                        value={this.state.languageUsed}
+                                        onChange={this.courseChange}
+                                        placeholder="languageUsed"
+                                        className={"bg-dark text-white"}/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridPrice">
+                                    <Form.Label>Price</Form.Label>
+                                    <Form.Control required
+                                        type="text" name="price"
+                                        value={this.state.price}
+                                        onChange={this.courseChange}
+                                        placeholder="Price"
+                                        className={"bg-dark text-white"}/>
+                                </Form.Group>
+                            </Form.Row>
+                        </Card.Body>
+                        <Card.Footer style={{"textAlign":"right"}}>
+                          <Button size="sm" variant="success" type="submit">
+                            <FontAwesomeIcon icon={faSave} /> Submit
+                          </Button>
+                        </Card.Footer>
+                    </Form>
+                </Card>
+            );
        }
 }
-
-export default Course;
--- src/main/webapp/reactjs/src/components/CourseList.js
+++ src/main/webapp/reactjs/src/components/CourseList.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Card,Table} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faList} from '@fortawesome/free-solid-svg-icons';
 
-class CourseList extends React.Component{
+export default class CourseList extends Component{
        render(){
-            return(<div>Course List</div>);
+            return(
+                <Card className={"border border-dark bg-dark text-white"}>
+                    <Card.Header><FontAwesomeIcon icon={faList} /> Course List</Card.Header>
+                    <Card.Body>
+                        <Table bordered hover striped variant="dark">
+                            <thead>
+                                <tr>
+                                  <th>Course Title</th>
+                                  <th>Course Code</th>
+                                  <th>Price</th>
+                                  <th>Language</th>
+                                  <th>Action</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr align="center">
+                                  <td colspan="6">No Courses Available</td>
+                                </tr>
+                              </tbody>
+
+                        </Table>
+                    </Card.Body>
+                </Card>
+
+            );
        }
 }
 
-export default CourseList;
